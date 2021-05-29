c137d908b5d403e80cae009c9fe38dbb65c40d26
tajinder-singh <tajinder.singh@outlook.in>
2021-05-30 00:41:03 +0530

    add new method tempConversion

--- src/main/java/sprcom/demo/MyController.java
+++ src/main/java/sprcom/demo/MyController.java
@@ -7,14 +7,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 
-    @GetMapping("/test")
-    public String getCapitalName(@RequestParam String name)
-    {
-       var capitalName = "Capital not found"; 
-       if(name.equals("India"))
-         capitalName =  "Delhi";
-       else if(name.equals("US"))
-         capitalName = "Washington";
-      return capitalName;   
-    }
+  @GetMapping("/staging")
+  public String getCapitalName(@RequestParam String name) {
+    var capitalName = "Capital not found";
+    if (name.equals("India"))
+      capitalName = "Delhi";
+    else if (name.equals("US"))
+      capitalName = "Washington";
+    return capitalName;
+  }
+
+  @GetMapping("/tempconversion")
+
+  public double tempConversion(@RequestParam double temperature, String unit) {
+    if (unit.equals("F"))
+      return (temperature - 32) * (5.0 / 9.0);
+    else
+      return (temperature * (9.0 / 5.0)) + 32;
+
+  }
+
 }
---
+++ src/test/java/TempConversiontest.java
@@ -0,0 +1,19 @@
+//package sprcom.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import sprcom.demo.MyController;
+
+public class TempConversiontest {
+    @Test
+    public void testTempConversion() throws Throwable {
+
+        MyController underTest = new MyController();
+        double temperature = 21.0;
+        String unit = "";
+        double result = underTest.tempConversion(temperature, unit);
+        assertEquals(68.0, result, "Test result failed");
+
+    }
+
+}
